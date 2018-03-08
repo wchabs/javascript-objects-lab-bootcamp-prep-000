f6ea2f666742bf68a0e081bbd6a0614a12a9ec49
William Chabla <w_chabla@hotmail.com>
2018-03-08 23:43:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-function destructivelyeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
