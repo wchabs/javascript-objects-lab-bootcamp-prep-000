4ce7d888b31ed0fcc2430d269af8a558257ba824
William Chabla <w_chabla@hotmail.com>
2018-03-08 23:32:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/* var recipes = {};
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  var newObj = Object.assign({}, object, {[key] = value});
+  return newObj;
 }
 
-function updateObjectWithKeyAndValue
+
 
