1469da9c8bebab8df702d941ac4a9a49362f9622
William Chabla <w_chabla@hotmail.com>
2018-03-08 23:40:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj[key];
+  return newObj;
 }
+
