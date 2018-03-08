2ce1024b0caca215f8d674a5cedb4a25bcb20a90
William Chabla <w_chabla@hotmail.com>
2018-03-08 23:36:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObj;
 }
 
-
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
 
